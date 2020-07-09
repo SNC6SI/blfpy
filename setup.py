87d0cf5885ea645f4ab0a9a9997d8818f3cf5d95
SNC6SI <shingosch@hotmail.com>
2020-07-09 23:54:34 +0800

    update setup & test

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup (name = 'blfpy',
        author = 'Shen, Chenghao',
        author_email='snc6si@gmail.com',
        description = 'blfpy in CPython',
-       data_files = [(sysconfig.get_python_lib(), ["binlog.dll"])],
+       # data_files = [(sysconfig.get_python_lib(), ["binlog.dll"])],
        ext_modules = [module1])
--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 import os
 import numpy
-import build.lib.win-amd64-3.7.blfpy as blfpy
+import blfpy
 
 #myfile = u"d:\\冬季试验数据\\401\\2019-12-26_02-55-04\\401_2019-12-26_10-20-04.blf"
 myfile = r"C:\D\02_MatlabRelevant\99_Playground\charge_time\0706\20200608_IC321_500_快充测试009.blf"
 myfile = myfile.encode('GBK')
 
 a = blfpy.readFileInfo(myfile)
+print(a)
 b = blfpy.readFileData(myfile)
+print(b)
 #c = pyBlfLoad.readFileData(myfile)
 
