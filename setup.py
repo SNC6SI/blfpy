45eca2befee49b54dc80fe80929968daea87947f
SNC6SI <shingosch@hotmail.com>
2020-07-10 11:07:07 +0800

    update setup

--- setup.py
+++ setup.py
@@ -4,14 +4,18 @@ import os
 
 mdl_blfpy = Extension('blfpy',
                       include_dirs = [os.path.join(sysconfig.get_python_lib(),
-                                                  'numpy', 'core', 'include')],
-                      libraries = ['binlog', 'python3'],
-                      sources = ['blfpy.c'])
+                                                  'numpy', 'core', 'include'),
+                                      os.path.join(os.getcwd(), 'blfpy')],
+                      library_dirs = [os.path.join(os.getcwd(), 'blfpy')],
+                      libraries = ['binlog'],
+                      sources = [os.path.join(os.getcwd(),
+                                              'blfpy', 'blfpy.c')])
 
 setup (name = 'blfpy',
        version = '0.1',
        author = 'Shen, Chenghao',
        author_email='snc6si@gmail.com',
        description = 'blfpy c-extension for python',
-       data_files = ["binlog.dll"],
-       ext_modules = [mdl_blfpy])
+       data_files = [os.path.join(os.getcwd(), 'blfpy', 'binlog.dll')],
+       ext_modules = [mdl_blfpy],
+       script_args = ['build', 'sdist', 'bdist'])
