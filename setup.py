53a2363b1f1cfcc9d487007a550990d84e85e3b4
SNC6SI <shingosch@hotmail.com>
2020-07-15 00:17:23 +0800

    update setup for renaming c module blfpy to blfc

--- setup.py
+++ setup.py
@@ -2,20 +2,24 @@ from distutils.core import setup, Extension
 from distutils import sysconfig
 import os
 
-mdl_blfpy = Extension('blfpy',
-                      include_dirs = [os.path.join(sysconfig.get_python_lib(),
-                                                  'numpy', 'core', 'include'),
-                                      os.path.join(os.getcwd(), 'blfpy')],
-                      library_dirs = [os.path.join(os.getcwd(), 'blfpy')],
-                      libraries = ['binlog'],
-                      sources = [os.path.join(os.getcwd(),
-                                              'blfpy', 'blfpy.c')])
+blfc = Extension('blfc',
+                 include_dirs = [os.path.join(sysconfig.get_python_lib(),
+                                              'numpy', 'core', 'include'),
+                                 os.path.join(os.getcwd(), 'src')],
+                 library_dirs = [os.path.join(os.getcwd(), 'src')],
+                 libraries = ['binlog'],
+                 sources = [os.path.join(os.getcwd(),
+                                         'src', 'blfc.c')])
 
-setup (name = 'blfpy',
-       version = '0.1',
+setup (name = 'blfc',
+       version = '0.1.0',
        author = 'Shen, Chenghao',
        author_email='snc6si@gmail.com',
+       maintainer = 'Shen, Chenghao',
+       maintainer_email = 'snc6si@gmail.com',
+       url = '',
        description = 'blfpy c-extension for python',
-       data_files = [os.path.join(os.getcwd(), 'blfpy', 'binlog.dll')],
-       ext_modules = [mdl_blfpy],
+       data_files = [os.path.join(os.getcwd(), 'src', 'binlog.dll')],
+       packages=['blfpy'],
+       ext_modules = [blfc],
        script_args = ['build', 'sdist', 'bdist'])
