179b850c9c9bd38453d284b0b987d5dfe067b28e
SNC6SI <shingosch@hotmail.com>
2020-07-14 23:51:45 +0800

    bug fix for getting abspath of __file__

--- test/test.py
+++ test/test.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import numpy
 from blfpy.blfload import blfload
 
