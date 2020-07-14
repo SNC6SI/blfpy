71105c1c9fd7f6b1a4bb2b10c7376cdbd909ece6
SNC6SI <shingosch@hotmail.com>
2020-07-14 20:55:03 +0800

    update test script

--- test/test.py
+++ test/test.py
@@ -1,14 +1,25 @@
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 import numpy
-import blfpy
+from blfpy.blfload import blfload
 
-#myfile = u"d:\\冬季试验数据\\401\\2019-12-26_02-55-04\\401_2019-12-26_10-20-04.blf"
-myfile = r"C:\D\02_MatlabRelevant\99_Playground\charge_time\0706\20200608_IC321_500_快充测试009.blf"
-myfile = myfile.encode('GBK')
-
-a = blfpy.readFileInfo(myfile)
-print(a)
-b = blfpy.readFileData(myfile)
-print(b)
-#c = pyBlfLoad.readFileData(myfile)
-
+if __name__ == "__main__":
+    bl = blfload(dbc=os.path.join(os.path.dirname(__file__), 
+                                  'dbc/IC321_PTCAN_CMatrix_V1.7_PT装车_VBU.dbc'),
+                 blf=os.path.join(os.path.dirname(__file__),
+                                  '20200608_IC321_500_快充测试009.blf'))
+    bl.signals = {'VBU_BMS_0x100': ['VBU_BMS_PackU',
+                                    'VBU_BMS_PackI',
+                                    'VBU_BMS_State'],
+                  'VBU_BMS_0x102': ['VBU_BMS_RealSOC',
+                                    'VBU_BMS_PackDispSoc'],
+                  'VBU_BMS_0x513': ['VBU_BMS_MaxTemp',
+                                    'VBU_BMS_MinTemp']}
+    # del bl.signals
+    # channel = None
+    bl.run_task()
+    # bl.plot(matlab.double(bl.can['VBU_BMS_0x100']['ctime'].tolist()),
+    #         matlab.double( bl.can['VBU_BMS_0x100']['VBU_BMS_PackU'].tolist()))
+    # bl.grid('on', nargout=0)
+    # bl.eng.exit()
