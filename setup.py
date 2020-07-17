ab2e026704f138c63dcbd99b3447c03450471205
SNC6SI <shingosch@hotmail.com>
2020-07-17 16:39:40 +0800

    make setup file clear

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ import os
 
 blfc = Extension(name = 'blfpy.blfc',
                  include_dirs = [os.path.join(sysconfig.get_python_lib(),
-                                              'numpy', 'core', 'include'),
-                                 os.path.join(os.getcwd(), 'src')],
+                                              'numpy', 'core', 'include')],
                  library_dirs = [os.path.join(os.getcwd(), 'src')],
                  libraries = ['binlog'],
                  sources = [os.path.join(os.getcwd(),
@@ -20,7 +19,7 @@ setup (name = 'blfpy',
        maintainer_email = 'snc6si@gmail.com',
        url = '',
        description = 'blfpy c-extension for python',
-       data_files = [os.path.join(os.getcwd(), 'src', 'binlog.dll')],
        packages=['blfpy'],
+       package_data = {'blfpy':['*.dll', '*.pyd']},
        ext_modules = [blfc],)
        # script_args = ['build_ext --inplace', 'sdist', 'bdist_wheel'])
