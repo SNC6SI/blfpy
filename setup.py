db7a04ce0ece95b91aa92bacb1da2ea5ba191b9a
SNC6SI <shingosch@hotmail.com>
2020-07-18 09:51:29 +0800

    version 0.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ blfc = Extension(name = 'blfpy.blfc',
                  libraries = ['binlog'])
 
 setup (name = 'blfpy',
-       version = '0.1.0',
+       version = '0.2.0',
        author = 'Shen, Chenghao',
        author_email='snc6si@gmail.com',
        maintainer = 'Shen, Chenghao',
