d00fcb084c32e20c973ba676958e39e82218ea30
SNC6SI <shingosch@hotmail.com>
2020-07-23 00:18:05 +0800

    draft mdfload, ignore mdf test file

---
+++ blfpy/mdfload.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 22 23:49:28 2020
+
+@author: SNC6SI
+"""
+import numpy as np
+
+
+if __name__ == "__main__":
+    mdf_file = r'../test/2020-07-17_19_IC321_HEV150_SW2.2.4_C2.2.1_FCH_NoreqI_01.dat'
+    infos = np.fromfile(mdf_file, dtype=np.byte)
