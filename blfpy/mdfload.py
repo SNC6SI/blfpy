6cc92699b6068ce2ac3daeb88685c3e1408d2b64
SNC6SI <shingosch@hotmail.com>
2020-07-23 10:30:38 +0800

    np.byte is an alias of np.int8, shall use np.uint8 instead

--- blfpy/mdfload.py
+++ blfpy/mdfload.py
@@ -9,4 +9,5 @@ import numpy as np
 
 if __name__ == "__main__":
     mdf_file = r'../test/2020-07-17_19_IC321_HEV150_SW2.2.4_C2.2.1_FCH_NoreqI_01.dat'
-    infos = np.fromfile(mdf_file, dtype=np.byte)
+    # np.byte is an alias of np.int8, shall use np.uint8 instead
+    data = np.fromfile(mdf_file, dtype=np.uint8)
